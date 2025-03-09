5957fe124148bd43a4cc03d2ddb99935031e0dca
Reema Radi <reema1radi1@outlook.com>
2025-03-09 13:35:21 +0300

    Created CSV file for bank project

---
+++ main.py
@@ -0,0 +1,16 @@
+import csv 
+
+fields = ["account_id", "first_name", "last_name", "password","balance_checking","balance_savings" ]
+
+rows = [ ["10001", "suresh", "sigera", "juagw362","1000","10000"],  
+         ["10002", "james", "taylor", "idh36%@#FGd","10000","10000"],  
+         ["10003", "melvin", "gordon", "uYWE732g4ga1","2000","20000"],  
+         ["10004", "stacey", "abrams", "DEU8_qw3y72$","2000","20000"],  
+         ["10005", "jake", "paul", "d^dg23g)@","100000","100000"]]
+
+filename="bank.csv"
+
+with open(filename, 'w') as csvfile:   
+    csvwriter = csv.writer(csvfile)  
+    csvwriter.writerow(fields)   
+    csvwriter.writerows(rows) 
